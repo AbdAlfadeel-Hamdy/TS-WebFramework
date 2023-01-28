82fc09f39bc4e8ac581384c7006b043b052d3ff2
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-01-28 17:13:36 +0200

    set method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "adham", age: 12 });
+user.set({ name: "Hossam" });
 
 console.log(user.get("name"));
 console.log(user.get("age"));
