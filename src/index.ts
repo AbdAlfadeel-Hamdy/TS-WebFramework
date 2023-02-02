65d2803894631c6de5e5ca4e9306d29fe6110b11
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-02 21:35:37 +0200

    Collection View

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
 
-const user = User.buildUser({ id: 2 });
-const root = document.querySelector("#root");
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else throw new Error(`Root Element not found`);
+const users = User.buildUserCollection();
+users.on("change", () => {
+  const root = document.getElementById("root");
+  if (root) {
+    const view = new UserList(root, users);
+    view.render();
+  }
+});
+
+users.fetch();
